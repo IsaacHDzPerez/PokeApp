19fb10e3740c1f75d1a2a0d51d5e2367ce53180f
Isaac <isaac.hdz.perez@gmail.com>
2024-09-04 09:52:16 -0600

    Initial Commit

---
+++ pokeapi/pokeapiApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokeapiApp.swift
+//  pokeapi
+//
+//  Created by Alumno on 04/09/24.
+//
+
+import SwiftUI
+
+@main
+struct pokeapiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
